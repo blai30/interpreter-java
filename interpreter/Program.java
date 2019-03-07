5478f863f78813cb063b71803e0a3ff5cf7807d8
Brian Lai <brianlai30@outlook.com>
2019-03-06 19:22:32 -0800

    Removed param comment from resolveAddrs javadoc comment in Program

--- interpreter/Program.java
+++ interpreter/Program.java
@@ -34,8 +34,6 @@ public class Program {
      * Currently all labels look like LABEL <<num>>>, these need to be converted into
      * correct addresses so the VirtualMachine knows what to set the Program Counter(PC)
      * HINT: make note what type of data-stucture bytecodes are stored in.
-     *
-//     * @param program Program object that holds a list of ByteCodes
      */
     public void resolveAddrs() {
         HashMap<String, Integer> addresses = new HashMap<>();
